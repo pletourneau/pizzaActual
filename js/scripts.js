68eff04fcab1450cb132296c7006242174f4aecc
Paul LeTourneau <pletourneau62@gmail.com>
2023-08-25 18:06:35 -0700

    add html context and print order summary

--- js/scripts.js
+++ js/scripts.js
@@ -28,19 +28,19 @@ Pizza.prototype.cost = function() {
       price += 1;
     }
   }
-  const dolla = this.price
+  const dolla = price;
+  console.log(price);
   return dolla;
 }
-
 
 // ui logic
 
 function handleFormSubmission(event) {
   event.preventDefault(event);
   addTopAndSize();
-
-
+  
 }
+
 
 
 function addTopAndSize() {
@@ -49,12 +49,12 @@ function addTopAndSize() {
   for (let i = 0; i < topps.length; i++) {
     toppings.push(topps[i].value);
   }
-console.log(toppings);
   const sml = document.querySelector('input[name="size"]:checked');
-  console.log(sml.value);
   const myPizza = new Pizza(toppings, sml.value);
-  myPizza.cost();
-  console.log(dolla);
+  
+
+  
+  document.querySelector("p").innerText = "Your Pizza will cost $ " + myPizza.cost() + ". You ordered a " + sml.value + " pizza with " + toppings.toString();
 }
 
 window.addEventListener("load", function() {
@@ -62,18 +62,4 @@ window.addEventListener("load", function() {
 });
 
 
-// let topps = 
-// [document.querySelector("input#btncheck1").checked,
-// document.querySelector("input#btncheck2").checked,
-// document.querySelector("input#btncheck3").checked,
-// document.querySelector("input#btncheck4").checked,
-// document.querySelector("input#btncheck5").checked,
-// document.querySelector("input#btncheck6").checked];
 
-// const arr =[];
-// for (let i=0; i < topps.length; i++) {
-//   if (topps[i] === true) {
-//     arr.push(topps.i);
-//   }
-//   console.log(arr);
-// }
